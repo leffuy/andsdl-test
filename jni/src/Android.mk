60848858516b898c8b8d9092338bcfe50a44404c
Stevie Frederick <leflor@gmail.com>
2012-02-13 01:39:26 -0500

    Can't forget the updates to the makefile.

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -14,8 +14,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.cpp \
 	main.c
 
-LOCAL_SHARED_LIBRARIES := SDL \
-	SDL_image
+LOCAL_SHARED_LIBRARIES := SDL SDL_image
 
 LOCAL_LDLIBS := -lGLESv1_CM -llog
 
