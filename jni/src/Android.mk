749d0f1ec5d977733d29531c29c9d567be95dc28
Stevie Frederick <leflor@gmail.com>
2012-04-03 21:26:33 -0400

    So we can build SDL_ttf

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -6,16 +6,17 @@ LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
 SDL_IMG_PATH := ../SDL_image
+SDL_TTF_PATH := ../SDL_ttf
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
-	$(LOCAL_PATH)/$(SDL_IMG_PATH)
+	$(LOCAL_PATH)/$(SDL_IMG_PATH) \
+	$(LOCAL_PATH)/$(SDL_TTF_PATH)
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.cpp \
-	main.c \
-        hashmap.c
+	main.c
 
-LOCAL_SHARED_LIBRARIES := SDL SDL_image
+LOCAL_SHARED_LIBRARIES := SDL SDL_image SDL2_ttf
 
 LOCAL_LDLIBS := -lGLESv2 -llog
 
